c7250ac777828d05074a6c14a18fcb52f8cc06fa
Shalyca Sottoriva <ssottori@c3r4s7.42london.com>
2024-03-20 17:14:42 +0000

    utils

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ssottori <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 15:44:01 by ssottori          #+#    #+#             */
-/*   Updated: 2024/03/20 15:45:52 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/03/20 17:12:35 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 
 void	child_process(char **av, char **envp, int fd);
 void	parent_process(char **av, char **envp, int fd);
-
-void	find_path(char *cmd, char **envp);
+char	*cmd_path(char *cmd, char **envp);
+void	exec_cmd(char *av, char **envp);
 void	p_error(void);
 
 #endif
--- utils.c
+++ utils.c
@@ -6,11 +6,17 @@
 /*   By: ssottori <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 15:39:08 by ssottori          #+#    #+#             */
-/*   Updated: 2024/03/20 16:16:23 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/03/20 17:14:05 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+void	p_error(void)
+{
+	perror("Error");
+	exit(EXIT_FALIURE);
+}
 
 void	child_process(char **av, char **envp, int *fd)
 {
@@ -22,6 +28,7 @@ void	child_process(char **av, char **envp, int *fd)
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(infile, STDIN_FILENO);
 	close(fd[1]);
+	exec_cmd(av[1], envp);
 }
 
 void	parent_process(char **av, char **envp, int *fd)
@@ -34,10 +41,29 @@ void	parent_process(char **av, char **envp, int *fd)
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(outfile, STDIN_FILENO);
 	close(fd[1]);
+	exec_cmd(av[1], envp);
 }
 
-void	p_error(void)
+char	*cmd_path(char **cmd, char **envp)
 {
-	perror("Error");
-	exit(EXIT_FALIURE);
+}
+
+void	exec_cmd(char *av, char **envp)
+{
+	char	**cmd;
+	char	*path;
+	int		i;
+
+	i = 0;
+	cmd = ft_split(av, ' ');
+	path = cmd_path(cmd, envp);
+	if (!path)
+	{
+		while (cmd[i++])
+			free(cmd[i]);
+		free(cmd);
+		p_error();
+	}
+	if (exceve(path, cmd, envp) == -1)
+		p_error();
 }
